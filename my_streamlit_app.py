899a9d526717a257742329cdc4b4e0b9429202d7
Narciso Alves <85737101+Narciso-A@users.noreply.github.com>
2022-11-23 10:44:32 +0100

    Add files via upload

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -5,18 +5,19 @@ st.title('Hello Wilders, welcome to my application!')
 
 st.write("I enjoy to discover stremalit possibilities")
 
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+#link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+link = 'https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv'
 df_weather = pd.read_csv(link)
 
 # Here we use "magic commands":
 df_weather
 
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+# st.line_chart(df_weather['MAX_TEMPERATURE_C'])
 
-import seaborn as sns
-viz_correlation = sns.heatmap(df_weather.corr(), 
-								center=0,
-								cmap = sns.color_palette("vlag", as_cmap=True)
-								)
+# import seaborn as sns
+# viz_correlation = sns.heatmap(df_weather.corr(), 
+# 								center=0,
+# 								cmap = sns.color_palette("vlag", as_cmap=True)
+# 								)
 
-st.pyplot(viz_correlation.figure)
+# st.pyplot(viz_correlation.figure)
