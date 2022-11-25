40341312d4bcc10366f95de873e7969eb018c953
Narciso Alves <85737101+Narciso-A@users.noreply.github.com>
2022-11-25 09:22:33 +0000

    Ajout commentaires

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -4,12 +4,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Entetes
 st.title('Cars')
 st.sidebar.image('voiture_clip_art_gratuit_dessin.png')
 
+# data !
 link = 'https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv'
 df_auto = pd.read_csv(link)
 
+# Selection continent
 continent_list = df_auto['continent'].unique()
 countries = st.sidebar.multiselect(
 	"Choix des continents", 
@@ -17,10 +20,12 @@ countries = st.sidebar.multiselect(
 	continent_list[0]
 	)
 
+# Table 
 df_auto_continent = df_auto[df_auto['continent'].isin(countries)]
 st.write('Table des valeurs')
 df_auto_continent
 
+# Matrice corrletion
 fig, ax = plt.subplots()
 plt.title('Correlation')
 ax = sns.heatmap(
@@ -30,11 +35,14 @@ ax = sns.heatmap(
 	annot=True
 	)
 st.pyplot(fig)
+st.write('Poids et cylindrée évoluent dans le meme sens')
 
+# Distribution poids
 fig, ax = plt.subplots()
 plt.title('Distribution')
 ax = sns.histplot(data=df_auto_continent, x='weightlbs')
 st.pyplot(fig)
+st.write('Les Poids se repartissent de 1500 à 5000 lbs')
 
 st.balloons()
 
